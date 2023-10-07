9783fab21e2d71889f4f4bf63d24f63aa4264719
Yashaswini192 <yashu19052@gmail.com>
2023-10-07 10:40:41 +0530

    [Yashaswini] Feat:Added Regular Expression and Validation in the Model

--- EmployeeMVC/ModelLayer/Model/EmployeeModel.cs
+++ EmployeeMVC/ModelLayer/Model/EmployeeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ModelLayer.Model
@@ -7,17 +8,20 @@ namespace ModelLayer.Model
     public class EmployeeModel
     {
         public int EmpId { get; set; }
-
+        
+        [RegularExpression("^[A-Z]{1}[a-z]{4,}",ErrorMessage ="Please Enter A Valid Name")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "{0} cannot be Empty")]
         public string ProfileImage { get; set; }
-
+        [Required(ErrorMessage = "{0} cannot be Empty")]
         public string Gender { get; set; }
-
+        [Required(ErrorMessage = "{0} cannot be Empty")]
         public string Department { get; set; }
 
+        [Required(ErrorMessage ="{0} cannot be Empty")]
+        [RegularExpression("^[0-9]{1,}$",ErrorMessage ="Please Enter A Valid Number")]
         public decimal Salary { get; set; }
-
+        [Required(ErrorMessage = "{0} cannot be Empty")]
         public DateTime StartDate { get; set; }
 
         public string Notes { get; set; }
